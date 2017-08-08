68fbfa3673fdd7cbcc8d8dd98aa55d8d8337dd05
Maxim Gorelikov <gorelikov.max@gmail.com>
2017-08-08 21:46:34 +0300

    simple stub

---
+++ src/main/java/ru/alfabank/workshop/ApplePayService.java
@@ -0,0 +1,11 @@
+package ru.alfabank.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplePayService {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplePayService.class, args);
+    }
+}
